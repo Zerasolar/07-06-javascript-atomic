51fc38005ed02443de2be2c18e37e704d8dac961
Kristin Houchin <kmhouchin@gmail.com>
2015-07-06 15:17:40 -0500

    Finish one, two and finishing three.

---
+++ js-atomic02/global.js
@@ -0,0 +1,8 @@
+alert("Hello friend, I'm Hal");
+
+console.log('Fried egg sandwhich');
+console.log('Had 4 years ago now 0');
+console.log('2 maybe');
+console.log('5. That would be great.');
+
+alert("Goodbye, so long....ect, ect");
---
+++ js-atomic03/global.js
@@ -0,0 +1,11 @@
+var yourname = "Kristin";
+var age = 27;
+var haircolor = "red/blond";
+
+alert("My name is " + yourname);
+alert("My age is " + age);
+alert("My hair color " + haircolor);
+
+console.log("My name is " + yourname);
+console.log("My age is " + age);
+console.log("My hair color " + haircolor);
